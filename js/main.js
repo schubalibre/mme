5320b6506d0e55b21648ee36d57a39eaa7627df4
schubalibre <robertodziuba@googlemail.com>
2015-10-31 22:10:15 +0100

    schrift footer anpassungen

--- js/main.js
+++ js/main.js
@@ -5,34 +5,35 @@
 
         var height = document.getElementById('article-top').offsetHeight;
         var scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+        var header = document.getElementsByTagName("header")[0];
 
         if (scrollTop > height) {
-            document.getElementsByTagName("header")[0].className = "active";
+            header.className = "active";
         } else if (scrollTop > 10) {
-            document.getElementsByTagName("header")[0].className = "transparent-active";
+            header.className = "transparent-active";
         } else {
-            document.getElementsByTagName("header")[0].className = "transparent";
+            header.className = "transparent";
         }
     };
 
     /*setTimeout(function () {
-        document.getElementById("helper-modal").style.display = "block";
-    }, 500);*/
+     document.getElementById("helper-modal").style.display = "block";
+     }, 500);*/
 
-    var tablefilter = new function() {
+    var tablefilter = new function () {
 
-        this.filter = function(){
+        this.filter = function () {
 
             var words = this.value.toLowerCase().split(" ");
             var table = document.getElementsByClassName(this.dataset.table);
             var ele;
 
-            for (var r = 1; r < table[0].rows.length; r++){
-                ele = table[0].rows[r].innerHTML.replace(/<[^>]+>/g,"");
+            for (var r = 1; r < table[0].rows.length; r++) {
+                ele = table[0].rows[r].innerHTML.replace(/<[^>]+>/g, "");
                 var className = 'hide';
 
                 for (var i = 0; i < words.length; i++) {
-                    if (ele.toLowerCase().indexOf(words[i])>=0)
+                    if (ele.toLowerCase().indexOf(words[i]) >= 0)
                         className = 'show';
                     else {
                         className = 'hide';
@@ -43,15 +44,14 @@
             }
         },
 
-        this.init = function(elementId){
-            var input =  document.getElementById(elementId);
-            input.addEventListener("keyup", this.filter, true);
-        }
+            this.init = function (elementId) {
+                var input = document.getElementById(elementId);
+                input.addEventListener("keyup", this.filter, true);
+            }
 
     };
 
     tablefilter.init("search-bar");
-
 
 
 })(document);
