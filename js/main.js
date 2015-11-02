badcb29c41d37498eeda880ab7bb8ecd099fb016
Robert Dziuba <robertodziuba@gmail.com>
2015-11-02 09:52:53 +0100

    Nav transparenz

---
+++ js/main.js
@@ -0,0 +1,16 @@
+window.onscroll = function(){
+
+    var scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.getElementById("article-top").offsetHeight;
+    var header = document.getElementsByTagName("header")[0];
+
+    console.log(scrollTop);
+    console.log(height);
+
+    if(scrollTop > height){
+        header.className = "small";
+    }else {
+        header.className = "";
+    }
+
+}
