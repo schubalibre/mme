f84375b49bd96474fa62d6e17d14685510d0916a
Inga <inga.schwarze2010@gmail.com>
2015-11-16 17:44:46 +0100

    formular-check code eingefügt; muss noch angepasst werden

--- js/main.js
+++ js/main.js
@@ -15,6 +15,58 @@ function init() {
 
 }
 
+
+
+
+
+
+var elem = document.getElementById('los');
+elem.addEventListener('click', checkFormular);
+
+function checkFormular() {
+	if (document.Formular.User.value == "") {
+		alert("Bitte Ihren Namen eingeben!");
+		document.Formular.User.focus();
+		return false;
+	}
+	if (document.Formular.Ort.value == "") {
+		alert("Bitte Ihren Wohnort eingeben!");
+		document.Formular.Ort.focus();
+		return false;
+	}
+	if (document.Formular.Mail.value == "") {
+		alert("Bitte Ihre E-Mail-Adresse eingeben!");
+		document.Formular.Mail.focus();
+		return false;
+	}
+	if (document.Formular.Mail.value.indexOf("@") == -1) {
+		alert("Keine E-Mail-Adresse!");
+		document.Formular.Mail.focus();
+		return false;
+	}
+	if (document.Formular.Alter.value == "") {
+		alert("Bitte Ihr Alter eingeben!");
+		document.Formular.Alter.focus();
+		return false;
+	}
+	var chkZ = 1;
+	for ( i = 0; i < document.Formular.Alter.value.length; ++i)
+		if (document.Formular.Alter.value.charAt(i) < "0" || document.Formular.Alter.value.charAt(i) > "9")
+			chkZ = -1;
+	if (chkZ == -1) {
+		alert("Altersangabe keine Zahl!");
+		document.Formular.Alter.focus();
+		return false;
+	}
+}
+
+
+
+
+
+
+
+
 // wenn wir über unser Titelbild herausscrollen wird die nav Leiste verkleinert
 function scrollNav() {
 
