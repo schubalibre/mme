fb41e42c577d9ecd81dd02f9d7dae49acdb32ea8
Inga <inga.schwarze2010@gmail.com>
2015-11-22 20:03:40 +0100

    Kommentare eingefügt

--- js/main.js
+++ js/main.js
@@ -4,89 +4,105 @@ window.addEventListener('load', init, false);
 // ruft alle weiteren Funktionen auf
 function init() {
 
-    var slider = new SliderCarousel({
-        elementId: "background-carousel",
-        autoSlide: true
-    });
+	// Slider-Carousel für Background wird initialisiert
+	new SliderCarousel({
+		elementId : "background-carousel",
+		autoSlide : true
+	});
+	
+	// Slider-Carousel für Bild-Upload wird initialisiert
+	new SliderCarousel({
+		elementId : "article-carousel",
+		autoSlide : false
+	});
 
-    var article = new SliderCarousel({
-        elementId: "article-carousel",
-        autoSlide: false
-    });
+	scrollNav(); // Startet Header-Scrollfunktion
 
-    scrollNav();
+	tablefilter.init("search-bar"); // Startet den Suchfilter für Artikeltabelle
+	
+	map.init(); // startet die Google-Map
 
-    tablefilter.init("search-bar");
+	// Alle Labels aus dem Formular in Variable speichern, um sie später zu durchlaufen
+	var labels = document.getElementsByTagName('LABEL');
 
-    //wir holen uns unser Formular
-    var form = document.getElementById("article-form");
-    // Browser-eigene Validation ausschalten da diese vor dem submit ausgefürht werden - um unsere eigenen Kontrollen durchzuführen
-    form.noValidate = true;
+	// Alle Labels werden durchlaufen
+	for (var i = 0; i < labels.length; i++) {
+		if (labels[i].htmlFor != '') {// haben Labels ein for mit ID des Input Feldes?
+			var elem = document.getElementById(labels[i].htmlFor);
+			// Wir holen uns das passende Input Feld
+			if (elem)// Gibt es Input Feld mit dieser ID?
+				elem.label = labels[i];
+			// Wenn ja: Wir erfahren, welches Label zu welchem Input Feld gehört
+		}
+	}
 
-    // beim Abschicken des Formulares wird unsere Funktion validateForm ausgeführt - um die eigenen Kontrollen durchzuführen
-    form.addEventListener("submit", validateForm);
+	//wir holen uns unser Formular
+	var form = document.getElementById("article-form");
+	// Browser-eigene Validation ausschalten da diese vor dem submit ausgefürht werden - um unsere eigenen Kontrollen durchzuführen
+	form.noValidate = true;
 
-    map.init();
+	// beim Abschicken des Formulares wird unsere Funktion validateForm ausgeführt - um die eigenen Kontrollen durchzuführen
+	form.addEventListener("submit", validateForm);
 }
 
 // wenn wir über unser Titelbild herausscrollen wird die nav Leiste verkleinert
 function scrollNav() {
 
-    var scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
-    var height = document.getElementById("article-top").offsetHeight;
-    var header = document.getElementsByTagName("header")[0];
+	var scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+	var height = document.getElementById("article-top").offsetHeight;
+	var header = document.getElementsByTagName("header")[0];
 
-    if (scrollTop > height) {
-        header.className = "small";
-    }
+	if (scrollTop > height) {
+		header.className = "small";
+	}
 
-    window.addEventListener("scroll", function () {
-        scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
-        if (scrollTop > height) {
-            header.className = "small";
-        } else {
-            header.className = "";
-        }
-    });
+	window.addEventListener("scroll", function() {
+		scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+		if (scrollTop > height) {
+			header.className = "small";
+		} else {
+			header.className = "";
+		}
+	});
 }
 
 // filtert die Tabele nach gesuchten Begriffe
 var tablefilter = new function () {
 
-    this.filter = function () {
+this.filter = function () {
 
-        var words = this.value.toLowerCase().split(" "),
-            table = document.getElementById(this.dataset.table),
-            ele = null;
+var words = this.value.toLowerCase().split(" "),
+table = document.getElementById(this.dataset.table),
+ele = null;
 
-        for (var r = 1; r < table.rows.length; r++) {
-            ele = table.rows[r].innerHTML.replace(/<[^>]+>/g, "");
-            var className = 'hide';
+for (var r = 1; r < table.rows.length; r++) {
+ele = table.rows[r].innerHTML.replace(/<[^>]+>/g, "");
+var className = 'hide';
 
-            for (var i = 0; i < words.length; i++) {
-                if (ele.toLowerCase().indexOf(words[i]) >= 0)
-                    className = 'show';
-                else {
-                    className = 'hide';
-                    break;
-                }
-            }
-            table.rows[r].className = className;
-        }
-    };
+for (var i = 0; i < words.length; i++) {
+if (ele.toLowerCase().indexOf(words[i]) >= 0)
+className = 'show';
+else {
+className = 'hide';
+break;
+}
+}
+table.rows[r].className = className;
+}
+};
 
-    this.init = function (elementId) {
-        var input = document.getElementById(elementId);
+this.init = function (elementId) {
+var input = document.getElementById(elementId);
 
-        var form = input.parentElement;
+var form = input.parentElement;
 
-        if (form.nodeName === "FORM") {
-            form.addEventListener('submit', function (event) {
-                event.preventDefault();
-                return false;
-            }, false);
-        }
+if (form.nodeName === "FORM") {
+form.addEventListener('submit', function (event) {
+event.preventDefault();
+return false;
+}, false);
+}
 
-        input.addEventListener("keyup", this.filter, true);
-    };
+input.addEventListener("keyup", this.filter, true);
+};
 };
--- js/validation.js
+++ js/validation.js
@@ -1,105 +1,98 @@
-
 function validateForm(event) {
+	
+	var scrolling = false; // wurde schon zu einem Element gescrollt?
 
-    var labels = document.getElementsByTagName('LABEL'),scrolling = false;
+	// Zugriff auf das event für alle Browser
+	event = ( event ? event : window.event);
 
-    // fetch cross-browser event object and form node
-    event = (event ? event : window.event);
+	// Zugriff auf das form-Element für alle Browser
+	var form = (event.target ? event.target : event.srcElement), f, field, formvalid = true;
 
-    var form = (event.target ? event.target : event.srcElement),
-        f, field, formvalid = true;
+	// Alle Input-Felder, Select, Textarea werden durchlaufen, für späteren Zugriff
 
-    // loop all fields
+	for ( f = 0; f < form.elements.length; f++) {
 
-    for (f = 0; f < form.elements.length; f++) {
+		// Holt sich das Feld an Stelle f
+		field = form.elements[f];
 
-        // get field
-        field = form.elements[f];
+		// Buttons etc. ignorieren
+		if (field.nodeName !== "INPUT" && field.nodeName !== "TEXTAREA" && field.nodeName !== "SELECT")
+			continue;
 
-        // ignore buttons, fieldsets, etc.
-        if (field.nodeName !== "INPUT" && field.nodeName !== "TEXTAREA" && field.nodeName !== "SELECT") continue;
+		// willValidate fragt ab, ob Browserspezifische Validierung vorhanden
+		// wenn existent, dann führe Validation aus
+		if ( typeof field.willValidate !== "undefined") {
+			// Überprüfung pro Feld starten
+			field.checkValidity();
+		}
+		
+		// Wir speichern erstes Element der Liste in span
+		var span = field.label.getElementsByClassName("error")[0];
+		
+		
+		if (span == undefined) { // noch kein span im Label
+			span = document.createElement("span"); // neues span kreieren
+			span.className = "error"; // bekommt class-name error
+			field.label.appendChild(span) // ins Label reinpacken
+		}
 
-        // is native browser validation available?
-        if (typeof field.willValidate !== "undefined") {
-            // native browser check
-            field.checkValidity();
-        }
+		span.innerHTML = ""; // span wird als leer überschrieben, weil evt. Validierung korrekt
+		field.className = ""; // class name "error" nicht mehr erforderlich, weil evt. Feld richtig
 
+		if (!field.validity.valid) { // immer noch nicht validiert?
 
-        for (var i = 0; i < labels.length; i++) {
-            if (labels[i].htmlFor != '') {
-                var elem = document.getElementById(labels[i].htmlFor);
-                if (elem)
-                    elem.label = labels[i];
-            }
-        }
-
-        var span = field.label.getElementsByClassName("error")[0];
-
-        if(span == undefined){
-            span = document.createElement("span");
-            span.className = "error";
-            field.label.appendChild(span)
-        }
-
-        span.innerHTML = "";
-
-        if (field.validity.valid) {
-
-            // remove error styles and messages
-
-            field.className = "";
-
-        } else {
-
-            // style field, show error, etc.
-
-            field.className = "error";
-
-            if (!scrolling) {
-                window.scroll(0, field.offsetTop - 120);
-                scrolling = true;
-            }
-
-            if(field.validity.valueMissing){
-                span.appendChild(document.createTextNode(" - " + field.dataset.errorValueMissing));
-            }else if(field.validity.patternMismatch){
-                span.appendChild(document.createTextNode(" - " + field.dataset.errorPatternMismatch));
-            }else if(field.validity.badInput){
-                span.appendChild(document.createTextNode(" - " + field.dataset.errorBadInput));
-            }else if(field.validity.customError){
-                span.appendChild(document.createTextNode(" - " + field.dataset.errorCustomError));
-            }else if(field.validity.stepMismatch){
-                span.appendChild(document.createTextNode(" - " + field.dataset.errorStepMismatch));
-            }else if(field.validity.rangeOverflow){
-                span.appendChild(document.createTextNode(" - " + field.dataset.errorRangeOverflow));
-            }else if(field.validity.rangeUnderflow){
-                span.appendChild(document.createTextNode(" - " + field.dataset.errorRangeUnderflow));
-            }else if(field.validity.patternMismatch){
-                span.appendChild(document.createTextNode(" - " + field.dataset.errorPatternMismatch));
-            }else if(field.validity.tooLong){
-                span.appendChild(document.createTextNode(" - " + field.dataset.errorTooLong));
-            }else if(field.validity.tooShort){
-                span.appendChild(document.createTextNode(" - " + field.dataset.errorTooShort));
-            }else if(field.validity.typeMismatch) {
-                span.appendChild(document.createTextNode(" - " + field.dataset.errorTypeMismatch));
-            }
-
-            // form is invalid
-            formvalid = false;
-        }
+			// Input-Feld / Select etc. bekommt class-name "error"
+			field.className = "error";
 
 
-        field.addEventListener("change",(function(event){
-            event.target.className = "";
-            event.target.label.getElementsByClassName("error")[0].innerHTML = "";
-        }),false);
+			// Möglicher Fehler werden abgefangen und mit Fehlermeldung ans Label gehangen
+			// Span Element mit Klasse error wird mit Error-Nachricht befüllt
+			// siehe HTML: z.B. data-error-value-missing
+			if (field.validity.valueMissing) {
+				span.appendChild(document.createTextNode(" - " + field.dataset.errorValueMissing));
+			} else if (field.validity.patternMismatch) {
+				span.appendChild(document.createTextNode(" - " + field.dataset.errorPatternMismatch));
+			} else if (field.validity.badInput) {
+				span.appendChild(document.createTextNode(" - " + field.dataset.errorBadInput));
+			} else if (field.validity.customError) {
+				span.appendChild(document.createTextNode(" - " + field.dataset.errorCustomError));
+			} else if (field.validity.stepMismatch) {
+				span.appendChild(document.createTextNode(" - " + field.dataset.errorStepMismatch));
+			} else if (field.validity.rangeOverflow) {
+				span.appendChild(document.createTextNode(" - " + field.dataset.errorRangeOverflow));
+			} else if (field.validity.rangeUnderflow) {
+				span.appendChild(document.createTextNode(" - " + field.dataset.errorRangeUnderflow));
+			} else if (field.validity.patternMismatch) {
+				span.appendChild(document.createTextNode(" - " + field.dataset.errorPatternMismatch));
+			} else if (field.validity.tooLong) {
+				span.appendChild(document.createTextNode(" - " + field.dataset.errorTooLong));
+			} else if (field.validity.tooShort) {
+				span.appendChild(document.createTextNode(" - " + field.dataset.errorTooShort));
+			} else if (field.validity.typeMismatch) {
+				span.appendChild(document.createTextNode(" - " + field.dataset.errorTypeMismatch));
+			}
 
-    }
+			if (!scrolling) { // Sprung zum ersten fehlerhaften Feld
+				window.scroll(0, field.offsetTop - 120); // nicht zu hoch (Header)
+				scrolling = true;
+			}
 
-    // cancel form submit if validation fails
-    if (!formvalid) {
-        if (event.preventDefault) event.preventDefault();
-    }
-    return formvalid;
-}
+			// Fomular soll nicht abgeschickt werden!
+			formvalid = false;
+		}
+
+		// Sobald zur Laufzeit erneut ins Feld geklickt wird, wird error-klasse und error-Nachricht gelöscht
+		field.addEventListener("change", (function(event) {
+			event.target.className = "";
+			event.target.label.getElementsByClassName("error")[0].innerHTML = "";
+		}), false);
+
+	}
+
+	// Das Formular wird nicht abgeschickt, wenn formvalid false ist
+	if (!formvalid) {
+		if (event.preventDefault) // Unterbinde die Absendefunktion an den Browser
+			event.preventDefault();
+	} // wenn Formular fehlerhaft = false
+	return formvalid;
+} 
