805a44034b58c95aac75a28d4cea649df8e2a393
Inga <inga.schwarze2010@gmail.com>
2015-11-22 20:32:01 +0100

    Weitere Kommentare eingefügt

--- js/main.js
+++ js/main.js
@@ -9,18 +9,21 @@ function init() {
 		elementId : "background-carousel",
 		autoSlide : true
 	});
-	
+
 	// Slider-Carousel für Bild-Upload wird initialisiert
 	new SliderCarousel({
 		elementId : "article-carousel",
 		autoSlide : false
 	});
 
-	scrollNav(); // Startet Header-Scrollfunktion
+	scrollNav();
+	// Startet Header-Scrollfunktion
 
-	tablefilter.init("search-bar"); // Startet den Suchfilter für Artikeltabelle
-	
-	map.init(); // startet die Google-Map
+	tablefilter.init("search-bar");
+	// Startet den Suchfilter für Artikeltabelle
+
+	map.init();
+	// startet die Google-Map
 
 	// Alle Labels aus dem Formular in Variable speichern, um sie später zu durchlaufen
 	var labels = document.getElementsByTagName('LABEL');
@@ -48,20 +51,20 @@ function init() {
 // wenn wir über unser Titelbild herausscrollen wird die nav Leiste verkleinert
 function scrollNav() {
 
-	var scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
-	var height = document.getElementById("article-top").offsetHeight;
-	var header = document.getElementsByTagName("header")[0];
+	var scrollTop = document.body.scrollTop || document.documentElement.scrollTop; // Scrollposition des Users
+	var height = document.getElementById("article-top").offsetHeight; // Höhe des Bildes holen
+	var header = document.getElementsByTagName("header")[0]; // Header holen
 
-	if (scrollTop > height) {
+	if (scrollTop > height) { // Scroll Position größer als das Bild -> small Klasse im CSS // wird beim Neuladen als erstes überprüft
 		header.className = "small";
 	}
 
-	window.addEventListener("scroll", function() {
-		scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+	window.addEventListener("scroll", function() { // Event reagiert auf Scrolling, wird immer wieder ausgeführt
+		scrollTop = document.body.scrollTop || document.documentElement.scrollTop; // Scroll Position holen
 		if (scrollTop > height) {
 			header.className = "small";
 		} else {
-			header.className = "";
+			header.className = ""; // falls User wieder über das Bild nach oben scrollt -> Header wieder groß
 		}
 	});
 }
@@ -69,40 +72,43 @@ function scrollNav() {
 // filtert die Tabele nach gesuchten Begriffe
 var tablefilter = new function () {
 
-this.filter = function () {
 
-var words = this.value.toLowerCase().split(" "),
-table = document.getElementById(this.dataset.table),
-ele = null;
+this.filter = function() {
 
-for (var r = 1; r < table.rows.length; r++) {
-ele = table.rows[r].innerHTML.replace(/<[^>]+>/g, "");
-var className = 'hide';
+	var words = this.value.toLowerCase().split(" "), // eingabe in words speichern als array
+	table = document.getElementById(this.dataset.table), // holt die Tabelle
+	ele = null; // Hilfsvariable
 
-for (var i = 0; i < words.length; i++) {
-if (ele.toLowerCase().indexOf(words[i]) >= 0)
-className = 'show';
-else {
-className = 'hide';
-break;
-}
-}
-table.rows[r].className = className;
-}
+	for (var r = 1; r < table.rows.length; r++) { // Zeilen durchlaufen
+		ele = table.rows[r].innerHTML.replace(/<[^>]+>/g, ""); // Alle tags löschen, Inhalt bleibt
+		var className = 'hide'; // Zeile verstecken
+
+		for (var i = 0; i < words.length; i++) { // Wörter im array durchlaufen
+			if (ele.toLowerCase().indexOf(words[i]) >= 0) // Wenn ein Wort in der Zeile existiert 
+				className = 'show'; // Klassenname = show
+			else {
+				className = 'hide'; // Klassenname = hide
+				break; // ein nicht gefundenes Wort: Zeile wird verborgen
+			}
+		}
+		table.rows[r].className = className; // Klassennamen vergeben
+	}
 };
 
-this.init = function (elementId) {
-var input = document.getElementById(elementId);
 
-var form = input.parentElement;
+this.init = function(elementId) {
+	var input = document.getElementById(elementId); // unsere search-bar
+	var form = input.parentElement; // unser Formular
 
-if (form.nodeName === "FORM") {
-form.addEventListener('submit', function (event) {
-event.preventDefault();
-return false;
-}, false);
-}
+	// Submit Abfangen und nicht Ausführen, egal ob Enter oder Suchen-Klick
+	if (form.nodeName === "FORM") {
+		form.addEventListener('submit', function(event) {
+			event.preventDefault();
+			return false;
+		}, false);
+	}
 
-input.addEventListener("keyup", this.filter, true);
-};
+	input.addEventListener("keyup", this.filter, true); // keyup event, this filter wird oben aufgerufen
+}; 
+
 };
