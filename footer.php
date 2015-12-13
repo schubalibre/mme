29ec9612288d24a58de8c362b5bfcd568fd31886
schubalibre <robertodziuba@googlemail.com>
2015-12-13 23:24:16 +0100

    jquery offline

--- footer.php
+++ footer.php
@@ -9,12 +9,11 @@
 ================================================== -->
 <!-- Placed at the end of the document so the pages load faster -->
 
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<!-- Include all compiled plugins (below), or include individual files as needed -->
-<script src="js/jquery-1.11.0.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/jquery-1.11.3.min.js"><\/script>')</script>
 <script src="js/bootstrap.min.js"></script>
 <script src="js/holder.min.js"></script>
+<script src="js/plugins.js"></script>
 <script src="js/main.js"></script>
 <script src="js/validation.js"></script>
 </body>
